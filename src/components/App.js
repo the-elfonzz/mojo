60e859793f7b1a7b241ebb021d42a2b6a6594548
Alexander Kostov <akexanderko@aiidatapro.com>
2018-10-11 18:11:44 +0300

    Add movies file;Add Object constructor in App file; To fix: returns empty objects

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,19 @@
 import React, { Component } from 'react';
 import Header from './Header';
 import Movie from './Movie';
+import {initialMovies} from '../movies'
+import {additionalMovies} from '../movies';
 import '../App.css';
 
 
 
 class App extends Component {
+  constructor() {
+      super();
+      this.state = {
+          movies: initialMovies
+      };
+  }
   render() {
     return (
       <div className="App">
@@ -14,10 +22,11 @@ class App extends Component {
           Welcome to the 'Movie Mojo' React app!
         </p>
         <div className="movies">
-          <Movie title="50 First Dates" year="2004" description="Henry Roth is a man afraid of commitment up until he meets the beautiful Lucy. They hit it off and Henry think he's finally found the girl of his dreams." poster="./posters/50-first-dates.png" />
-          <Movie title="Ferris Bueller's Day Off" year="1986" description="A high school wise guy is determined to have a day off from school, despite what the principal thinks of that." poster="./posters/ferris.png" />
-          <Movie title="Matilda" year="1996" description="Story of a wonderful little girl, who happens to be a genius, and her wonderful teacher vs. the worst parents ever and the worst school principal imaginable." poster="./posters/matilda.png" />
-          <Movie title="Dirty Dancing" year="1987" description="Spending the summer at a Catskills resort with her family, Frances 'Baby' Houseman falls in love with the camp's dance instructor, Johnny Castle." poster="./posters/dirty-dancing.png" />
+          {
+              Object
+                .keys(this.state.movies)
+                .map(key => <Movie key={key} meta={this.state.movies} />)
+          }
         </div>
       </div>
     );
--- src/components/Movie.js
+++ src/components/Movie.js
@@ -5,7 +5,6 @@ class Movie extends Component {
 		return(
 			<div className="movie">
 			  <h2>{ this.props.title }</h2>
-			  <h2>{ this.props.title }</h2>
 			<div><img width="200" src={ this.props.poster } /></div>
 			  <p>({ this.props.year })</p>
 			  <p>{ this.props.description }</p>
