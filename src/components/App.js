a15680bb6c525b861b25a3be62c1656b8c4775be
Alexander Kostov <akexanderko@aiidatapro.com>
2018-10-11 15:38:05 +0300

    Add Movie Component

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Header from './Header';
+import Movie from './Movie';
 import '../App.css';
 
 
@@ -12,6 +13,12 @@ class App extends Component {
         <p className="App-intro">
           Welcome to the 'Movie Mojo' React app!
         </p>
+        <div className="movies">
+          <Movie title="50 First Dates" year="2004" description="Henry Roth is a man afraid of commitment up until he meets the beautiful Lucy. They hit it off and Henry think he's finally found the girl of his dreams." poster="./posters/50-first-dates.png" />
+          <Movie title="Ferris Bueller's Day Off" year="1986" description="A high school wise guy is determined to have a day off from school, despite what the principal thinks of that." poster="./posters/ferris.png" />
+          <Movie title="Matilda" year="1996" description="Story of a wonderful little girl, who happens to be a genius, and her wonderful teacher vs. the worst parents ever and the worst school principal imaginable." poster="./posters/matilda.png" />
+          <Movie title="Dirty Dancing" year="1987" description="Spending the summer at a Catskills resort with her family, Frances 'Baby' Houseman falls in love with the camp's dance instructor, Johnny Castle." poster="./posters/dirty-dancing.png" />
+        </div>
       </div>
     );
   }
---
+++ src/components/Movie.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class Movie extends Component {
+	render() {
+		return(
+			<div className="movie">
+			  <h2>{ this.props.title }</h2>
+			  <h2>{ this.props.title }</h2>
+			<div><img width="200" src={ this.props.poster } /></div>
+			  <p>({ this.props.year })</p>
+			  <p>{ this.props.description }</p>
+			</div>
+		)
+	}
+}
+
+export default Movie;
